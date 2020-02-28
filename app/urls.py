0e6f54eb708a8c6c8d61a8f38373542ea5e2db2e
privera <privera@celerity.com>
2020-02-27 19:56:05 -0500

    add missing tinymce urls

--- app/urls.py
+++ app/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
+    path(r'^tinymce/', include('tinymce.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
