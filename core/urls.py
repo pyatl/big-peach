e94002d38f46a16c8703ec6c107005243c7c6c1e
privera <privera@celerity.com>
2020-03-16 17:49:45 -0400

    update dir structures, add django toolbar, environs

--- core/urls.py
+++ core/urls.py
@@ -3,10 +3,7 @@ from django.urls import path, include
 from .views import IndexView
 
 urlpatterns = [
-    
     path('events/', include('events.urls')),
-    
-    
     path('', IndexView.as_view(),
         name='index'),
 ]
--- urls.py
+++ urls.py
@@ -22,5 +22,11 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
-    path(r'^tinymce/', include('tinymce.urls')),
+    path(r'tinymce/', include('tinymce.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
