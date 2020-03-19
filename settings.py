8f12e6a424b552a1df9a5a1d4f39bd6b6c1c9a81
privera <privera@celerity.com>
2020-03-19 19:14:09 -0400

    add setup stat static files serving

--- settings.py
+++ settings.py
@@ -122,6 +122,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'core/static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
 # TinyMCE
 
 TINYMCE_DEFAULT_CONFIG = {'theme': 'advanced', }
