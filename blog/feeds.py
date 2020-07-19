3f4316d5ee2b805fa4d4d5d3d367f19a3d12d306
Doug Hellmann <doug@doughellmann.com>
2020-07-19 08:33:34 -0400

    blog: add rss feed for all entries
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

---
+++ blog/feeds.py
@@ -0,0 +1,28 @@
+from django.contrib.syndication.views import Feed
+from django.urls import reverse
+
+from blog.models import PostStatus, PUBLISHED
+
+
+class LatestEntriesFeed(Feed):
+    title = "PyATL news"
+    link = "/"
+    description = "Latest blog posts from PyATL.dev"
+
+    def items(self):
+        return PostStatus.objects.filter(status=PUBLISHED)
+
+    def item_title(self, item):
+        return item.post.title
+
+    def item_description(self, item):
+        return item.post.body
+
+    def item_link(self, item):
+        return reverse(
+            'blog_post_detail',
+            args=[
+                item.post.slug,
+                item.post.pk,
+            ],
+        )
