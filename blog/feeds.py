9e885475c38f8984e898f7df4b6b469d9653e686
Doug Hellmann <doug@doughellmann.com>
2020-07-19 12:16:50 -0400

    blog: use get_absolute_url() methods instead of computing URL in feed
    This makes the logic for computing the URL of a blog post more reusable.
    
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- blog/feeds.py
+++ blog/feeds.py
@@ -17,12 +17,3 @@ class LatestEntriesFeed(Feed):
 
     def item_description(self, item):
         return item.post.body
-
-    def item_link(self, item):
-        return reverse(
-            'blog_post_detail',
-            args=[
-                item.post.slug,
-                item.post.pk,
-            ],
-        )
