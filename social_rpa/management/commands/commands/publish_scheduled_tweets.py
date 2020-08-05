59b50f5ade162c9162a8a8f3cb7d7f77d3b54830
pryelluw <pryelluw@gmail.com>
2020-08-04 22:03:26 -0400

    put back file accidentally deleted in last commit

---
+++ social_rpa/management/commands/commands/publish_scheduled_tweets.py
@@ -0,0 +1,42 @@
+import logging
+import arrow
+from django.utils import timezone
+from django.core.management.base import BaseCommand
+from social_rpa.twitter import authenticate, update_status
+from social_rpa.models import ScheduledTweet
+
+logged = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = 'Publish scheduled tweets to Twitter'
+
+    def handle(self, *args, **options):
+
+        consumer_key = ''
+        consumer_secret = ''
+        access_token = ''
+        access_token_secret = ''
+
+        api = authenticate(consumer_key, consumer_secret, access_token, access_token_secret)
+
+        arw = arrow.utcnow()
+        from_time = arw.shift(hours=-1).datetime
+        to_time = timezone.now()
+
+        scheduled_tweets = ScheduledTweet.objects.filter(
+            published=False).filter(
+                scheduled_for__gte=from_time).filter(
+                    scheduled_for__lte=to_time)
+
+        if scheduled_tweets:
+            for scheduled_tweet in scheduled_tweets:
+                successful = update_status(api, scheduled_tweet.tweet.content)
+                if successful:
+                    scheduled_tweet.published = True
+                    scheduled_tweet.save()
+                    logger.info(f'Published scheduled tweet with pk: {scheduled_tweet.pk}')
+                else:
+                    logger.error(f'Error publishing scheduled tweet with pk: {scheduled_tweet.pk}')
+
+        logger.info('publish scheduled tweets command ran successfully')
