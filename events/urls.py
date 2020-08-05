376544a84d6ec64373d8554d6bffe5e5964f1fc6
pryelluw <pryelluw@gmail.com>
2020-08-04 20:39:33 -0400

    refactor event urls

--- events/urls.py
+++ events/urls.py
@@ -11,28 +11,23 @@ from events.feeds import Calendar
 slug = '(?P<slug>[-\\w\\d]+)'
 pk = '(?P<pk>[0-9]+)'
 date = '(?P<date>[0-9]{4}-?[0-9]{2}-?[0-9]{2})'
+event_path = f'{date}/{slug}/{pk}/$'
 
 urlpatterns = [
     # location
-    re_path(r'^location/{slug}/{pk}/$'.format(
-        slug=slug,
-        pk=pk),
+    path('location/<slug:slug>/<int:pk>/',
         LocationView.as_view(),
         name='location'),
-    re_path(r'^locations/$', LocationsView.as_view(), name='locations'),
+    path('locations/', LocationsView.as_view(), name='locations'),
 
     # event
-    re_path(r'^{date}/{slug}/{pk}/$'.format(
-        date=date,
-        slug=slug,
-        pk=pk),
+    re_path(r'^{}'.format(event_path),
         EventView.as_view(),
         name='event'),
-    re_path(r'^invite/{pk}/$'.format(
-        pk=pk),
+    path('invite/<int:pk>/',
         EventInviteDownloadView.as_view(),
         name='event-invite'),
-    re_path(r'^$', EventsView.as_view(), name='events'),
+    path('', EventsView.as_view(), name='events'),
 
     # calendar
     path('feed/ical/', Calendar(), name='calendar'),
